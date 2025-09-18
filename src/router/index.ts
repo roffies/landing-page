651e859af5a7f17ff123304cc9862089a99e692e
Deskjobo <johan.bottger@gmail.com>
2025-09-18 18:01:34 -0500

    fix deploy 2

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,7 +12,7 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
-    scrollBehavior(to, from, savedPosition) {
+    scrollBehavior(_to, _from, savedPosition) {
         if (savedPosition) return savedPosition
         return { top: 0 }
     }
