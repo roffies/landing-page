ced3fb8049e7d9d9a71995aaf5c3ba17018560b6
Deskjobo <johan.bottger@gmail.com>
2025-09-18 17:20:01 -0500

    Configurar github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/',
+  base: '/landing-page/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
