e35497aba3b26c3720bb9594696b361f28571b31
Piero Sulca <psulcasanchez@gmail.com>
2025-10-03 23:25:34 -0500

    feat: integrate team and FAQ sections into landing page

--- src/views/app-landing.page.vue
+++ src/views/app-landing.page.vue
@@ -4,6 +4,8 @@ import Footer from "@/components/footer-section.component.vue"
 import HeroSection from "@/components/hero-section.component.vue"
 import HowItWorksSection from "@/components/how-it-works-section.component.vue"
 import BenefitsSection from "@/components/benefits-section.component.vue"
+import TeamSection from "@/components/team-section.component.vue"
+import FaqSection from "@/components/faq-section.component.vue"
 </script>
 
 <template>
@@ -11,6 +13,8 @@ import BenefitsSection from "@/components/benefits-section.component.vue"
   <HeroSection />
   <HowItWorksSection />
   <BenefitsSection />
+  <TeamSection />
+  <FaqSection />
   <Footer />
 </template>
 
